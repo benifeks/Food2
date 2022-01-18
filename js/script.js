4a51fd806b2600b8c39426ee59ae0892705adff5
benifeks <benifeks72@gmail.com>
2022-01-18 12:34:54 +0200

    Промисы при отправке формы

--- js/script.js
+++ js/script.js
@@ -90,7 +90,6 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-
     setClock('.timer', deadline);
 
     // ****** modal ********
@@ -213,7 +212,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Forms ************************************
 
     const forms = document.querySelectorAll('form');
-        
+
     const message = {
         loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
@@ -237,30 +236,29 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);//form.append(statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
             const object = {};
-            formData.forEach(function(value, key){
+            formData.forEach(function (value, key) {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    statusMessage.remove();
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)    
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();               
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
@@ -288,4 +286,5 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-}); //*** */
+
+}); 
